1fac0c08584963b9823956e41e1f311b332dd7ee
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2026-06-20 15:09:42 -0400

    Fresh EPG Update: Sat Jun 20 19:12:58 UTC 2026

--- generate_epg.py
+++ generate_epg.py
@@ -22,7 +22,7 @@ def generate_multi_channel_epg():
     ]
     
     CUSTOM_MESSAGES = [
-        "(TVP) = TVPASS IS DOWN",
+        "(TVP) = TVPASS REMOVED",
         "(CC) = CORDCUTTER TV",
         "(KSTV) = KSTV",
         "(PTV) = PEAKY TV"
@@ -56,7 +56,7 @@ def generate_multi_channel_epg():
             stop_str = prog_stop.strftime('%Y%m%d%H%M%S +0000')
             
             prog = ET.SubElement(tv, 'programme', start=start_str, stop=stop_str, channel=ch["id"])
-            ET.SubElement(prog, 'title', lang="en").text = "TVPass removed." 
+            ET.SubElement(prog, 'title', lang="en").text = "Added Whiplash TV, Adult Channels and testing a new source for LIVE TV." 
             # Description now only contains the custom messages
             full_description = "\n".join(CUSTOM_MESSAGES)
             ET.SubElement(prog, 'desc', lang="en").text = full_description
